f2eb4c76630e9ed5742da7cf4cd4149f291b86f4
angel_374 <angel.ortega@jala.university>
2025-02-13 12:41:58 -0500

    🏗️ final changes of model

--- pages/Team.py
+++ pages/Team.py
@@ -1,7 +1,54 @@
 import streamlit as st
-st.set_page_config(page_title="Team Demo", page_icon="assets/LogoWithoutBg.png")
+
+st.set_page_config(page_title="Team", page_icon="assets/LogoWithoutBg.png")
 
 
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
+st.title("👨‍💻 Conoce nuestro equipo de desarrollo")
 
+
+st.markdown(
+    """
+    <style>
+        @keyframes scroll {
+            0% { transform: translateY(100%); }
+            100% { transform: translateY(-100%); }
+        }
+
+        .credit-container {
+            height: 400px; 
+            overflow: hidden;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: transparent;
+            color: white;
+            font-size: 24px;
+            font-weight: bold;
+            position: relative;
+        }
+
+        .credit-text {
+            display: block;
+            position: absolute;
+            animation: scroll 20s linear infinite;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <div class="credit-container">
+        <div class="credit-text">
+            👨‍💻 Zaid Pantoja - administrador del modelo de machine learning <br><br>
+            🖥️ Juan Jose Monsalve - implementacion de deepseek <br><br>
+            📊 Angel Ortega - despliegue del proyecto y Charts de los datasets <br><br>
+            💻 Santiago Cardenas Jotty - presentacion de equipo y documentacion <br><br>
+            📜 Andres Aviles - mapa de la localidad resaltando su numero de ranking <br><br><br><br><br>
+            ⚡ Derechos reservados - Los Matemonda 2024
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
